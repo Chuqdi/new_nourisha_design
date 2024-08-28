e1f8f9f8107e3a48186dd45c99d0385b1f3406f2
Chuqdi <morganhezekiah111@gmail.com>
2024-08-28 09:09:40 +0100

    g

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -31,10 +31,10 @@ export default function AboutPage() {
     <div className="w-full h-full relative pt-6">
       <Navbar />
 
-      <div className="mx-6.25 flex flex-col gap-8 mt-32">
+      <div className="mx-1.25 md:mx-6.25 flex flex-col gap-8 mt-32">
         <MessageBtn title="ABOUT US" />
         <div>
-          <h4 className="text-[#7DB83A] text-center font-NewSpiritBold text-[4rem] ">
+          <h4 className="text-[#7DB83A] text-center font-NewSpiritBold text-[2rem] md:text-[4rem] ">
             Welcome to Nourisha
           </h4>
           <p className="text-black-900 text-center font-inter text-base">
@@ -50,10 +50,10 @@ export default function AboutPage() {
       </div>
       <img src="/images/zigzag.png" className="w-full" />
 
-      <div className="flex flex-col md:flex-row gap-4 mx-6.25 my-[2rem]">
+      <div className="flex flex-col md:flex-row gap-4 mx-1.25 md:mx-6.25 my-[2rem]">
         {abouts.map((about, index) => (
           <div className="flex-1 gap-4 flex flex-col" key={`about_${index}`}>
-            <h4 className="text-[#7DB83A] text-[2rem] font-NewSpiritBold">
+            <h4 className="text-[#7DB83A] text-[1.5rem] md:text-[2rem] font-NewSpiritBold">
               {about.title}
             </h4>
             <p className="text-black-900 text-base tracking-[-0.015rem] leading-[1.5rem] ">
@@ -63,7 +63,7 @@ export default function AboutPage() {
         ))}
       </div>
 
-      <div className="px-6.25 flex flex-col gap-3 bg-[#F9FAFB] py-6.25">
+      <div className="px-1.25 md:px-6.25 flex flex-col gap-3 bg-[#F9FAFB] py-6.25">
         <div className="flex justify-between items-center">
           <button className="text-[#FE7E00] font-inter text-base leading-[1.5rem] tracking-[-0.015rem] rounded-full p-6 h-14  w-fit border-[1px] border-[#828893] flex justify-center items-center">
             OUR FEATURES
@@ -82,7 +82,7 @@ export default function AboutPage() {
             </button>
           </div> */}
         </div>
-        <h3 className="text-black-900 font-NewSpiritBold text-[2rem]">
+        <h3 className="text-black-900 font-NewSpiritBold text-[1.4rem] md:text-[2rem]">
           Explore What Nourisha Offers
         </h3>
       </div>
