26748719745fad19c9355f916a2c7fb7846e9148
Chuqdi <morganhezekiah111@gmail.com>
2024-09-10 10:23:01 +0100

    g

--- src/components/sections/TestmoniesSection.tsx
+++ src/components/sections/TestmoniesSection.tsx
@@ -24,30 +24,30 @@ const responsive = {
 
 const firstTestmonies = [
   {
-    name:"Earl Joey",
-    description:`They${"’"}re really nice above anything else. Every time I have had issues with my order they have resolved it speedily and considerately.",
+    name: "Earl Joey",
+    description: `They${"’"}re really nice above anything else. Every time I have had issues with my order they have resolved it speedily and considerately.",
     date:"25 Apr 2024`,
-    caption:`They${"’"}re really nice above anything…`,
+    caption: `They${"’"}re really nice above anything…`,
   },
   {
-    name:"Izek Precious",
-    description:`Nourisha is 100 percent splendid….. quick delivery on your choose date and the meal is a top notch… can${"'"}t wait to order again…❤️`,
-    date:"9 Apr 2024",
-    caption:`Excellent`,
+    name: "Izek Precious",
+    description: `Nourisha is 100 percent splendid….. quick delivery on your choose date and the meal is a top notch… can${"'"}t wait to order again…❤️`,
+    date: "9 Apr 2024",
+    caption: `Excellent`,
   },
   {
-    name:"Iphie",
-    description:`Swift deliver, well packaged meals and very loverly customer service. The food itself, was delicious. Will definitely order again`,
-    caption:`Excellent`,
-    date:"5 Jun 2024"
+    name: "Iphie",
+    description: `Swift deliver, well packaged meals and very loverly customer service. The food itself, was delicious. Will definitely order again`,
+    caption: `Excellent`,
+    date: "5 Jun 2024",
   },
   {
-    name:"Freeborn Ehirhere",
-    description:`Food was amazing, great customer service and support, fast delivery. I totally rey`,
-    caption:`Food was amazing`,
-    date:"5 June 2024"
-  }
-]
+    name: "Freeborn Ehirhere",
+    description: `Food was amazing, great customer service and support, fast delivery. I totally rey`,
+    caption: `Food was amazing`,
+    date: "5 June 2024",
+  },
+];
 
 export default function TestmoniesSection() {
   const isMobile = useMediaQuery({ maxWidth: BREAKPOINT });
@@ -61,9 +61,7 @@ export default function TestmoniesSection() {
           <div className="flex items-center gap-3">
             <Ratings />
           </div>
-          <p className="text-sm font-inter font-bold">
-            {testmonies?.caption}
-          </p>
+          <p className="text-sm font-inter font-bold">{testmonies?.caption}</p>
           <p className="text-black-900 font-inter text-sm">
             {testmonies?.description}
           </p>
@@ -75,11 +73,7 @@ export default function TestmoniesSection() {
       ))}
     </div>
   ) : (
-    <Carousel
-      responsive={responsive}
-      autoPlay
-      swipeable
-    >
+    <Carousel responsive={responsive} autoPlay swipeable>
       {firstTestmonies.map((testmonies, index) => (
         <div
           key={`carousel_item_${index}`}
@@ -88,9 +82,7 @@ export default function TestmoniesSection() {
           <div className="flex items-center gap-3">
             <Ratings />
           </div>
-          <p className="text-sm font-inter font-bold">
-            {testmonies?.caption}
-          </p>
+          <p className="text-sm font-inter font-bold">{testmonies?.caption}</p>
           <p className="text-black-900 font-inter text-sm">
             {testmonies?.description}
           </p>
@@ -101,7 +93,7 @@ export default function TestmoniesSection() {
         </div>
       ))}
 
-      {[1, 2, 3].map((_, index) => (
+      {firstTestmonies.map((testmonies, index) => (
         <div
           key={`item_${index}`}
           className="w-[25rem]  mx-4 bg-[#F2F4F7] rounded-[0.375rem] p-2 gap-3 flex flex-col"
@@ -109,17 +101,13 @@ export default function TestmoniesSection() {
           <div className="flex items-center gap-3">
             <Ratings />
           </div>
-          <p className="text-sm font-inter font-bold">
-            They’re really nice above anything…
-          </p>
+          <p className="text-sm font-inter font-bold">{testmonies.caption}</p>
           <p className="text-black-900 font-inter text-sm">
-            They’re really nice above anything else. Every time I have had
-            issues with my order they have resolved it speedily and
-            considerately.
+            {testmonies.description}
           </p>
           <div className="font-inter text-sm text-black-900 gap-2 flex ">
-            <span className="font-bold">Earl Joey</span>
-            25 Apr 2024
+            <span className="font-bold">{testmonies.name}</span>
+            {testmonies.date}
           </div>
         </div>
       ))}
