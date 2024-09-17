3b44e7ab8e3e797b937d73949b0ebcce4abc6f8e
Chuqdi <morganhezekiah111@gmail.com>
2024-09-17 17:26:37 +0100

    g

--- src/HOC/UserContext.tsx
+++ src/HOC/UserContext.tsx
@@ -20,37 +20,37 @@ function UserContextProvider({ children }: { children: React.ReactNode }) {
   const { toast } = useToast();
   const { axiosClient } = useAuth();
 
-  const fetchUser = async () => {
-    return axiosClient.get("customers/me");
-  };
+  // const fetchUser = async () => {
+  //   return axiosClient.get("customers/me");
+  // };
 
-  const { data, isLoading, isError } = useQuery(
-    queryKeys.AUTH_USER_ME,
-    fetchUser,
-    {
-      cacheTime: 1000,
-      retry:false,
-    }
-  );
+  // const { data, isLoading, isError } = useQuery(
+  //   queryKeys.AUTH_USER_ME,
+  //   fetchUser,
+  //   {
+  //     cacheTime: 1000,
+  //     retry:false,
+  //   }
+  // );
 
-  useEffect(() => {
-    if (data?.data?.data) {
-      setUser(data?.data?.data);
-    }
-  }, [data]);
+  // useEffect(() => {
+  //   if (data?.data?.data) {
+  //     setUser(data?.data?.data);
+  //   }
+  // }, [data]);
 
-  useEffect(() => {
-    // if (isError) {
-    //   toast({
-    //     variant: "destructive",
-    //     title: "Authentication failed",
-    //     description: "Authentication failed",
-    //   });
-    //   if (!window.location.href.includes("auth") && window.location.href === "/") {
-    //     // window.location.href = "/auth";
-    //   }
-    // }
-  }, [isError]);
+  // useEffect(() => {
+  //   // if (isError) {
+  //   //   toast({
+  //   //     variant: "destructive",
+  //   //     title: "Authentication failed",
+  //   //     description: "Authentication failed",
+  //   //   });
+  //   //   if (!window.location.href.includes("auth") && window.location.href === "/") {
+  //   //     // window.location.href = "/auth";
+  //   //   }
+  //   // }
+  // }, [isError]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
