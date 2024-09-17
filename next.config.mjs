f6f9a21b4d19aa0b7dccd0717de6832a2c1e51b3
Chuqdi <morganhezekiah111@gmail.com>
2024-09-17 10:37:23 +0100

    g

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
     API_URL: "https://api.eatnourisha.com/v1/",
     STRIPE_PK_TEST: "pk_test_CTdNlDzkd3JhDT8yof3Hdw5B",
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
