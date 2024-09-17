8e7ad9dfa23782b5090948aa9f1392da15da1d45
Chuqdi <morganhezekiah111@gmail.com>
2024-09-17 11:34:38 +0100

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  onError: (error) => {
-    if (process.env.NEXT_IGNORE_BUILD_ERROR === "true") {
-      console.warn("Ignoring build error:", error);
-      return;
-    }
-    throw error;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   env: {
     API_URL: "https://api-stage.eatnourisha.com/v1/",
     // API_URL: "https://api.eatnourisha.com/v1/",
