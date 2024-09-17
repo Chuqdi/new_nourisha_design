16c579052176d68899b786c991ede88d34de5414
Chuqdi <morganhezekiah111@gmail.com>
2024-09-17 17:48:39 +0100

    g

--- src/HOC/UserContext.tsx
+++ src/HOC/UserContext.tsx
@@ -7,6 +7,7 @@ import Logo from "@/components/ui/Logo";
 import { useQuery } from "react-query";
 import useAuth from "@/hooks/useAuth";
 import dynamic from "next/dynamic";
+import axios from "axios";
 
 export const UserContext = createContext<
   | {
@@ -19,6 +20,9 @@ export const UserContext = createContext<
 function UserContextProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<IUser>({} as IUser);
   const { toast } = useToast();
+  const axiosClient = axios.create({
+    baseURL: `${process.env.API_URL}/`,
+  });
   // const { axiosClient } = useAuth();
  
 
