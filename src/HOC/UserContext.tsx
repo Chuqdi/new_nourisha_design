f7356e461a2365d80f3d652e1ff9625684e0042f
Chuqdi <morganhezekiah111@gmail.com>
2024-09-17 17:24:16 +0100

    g

--- src/HOC/UserContext.tsx
+++ src/HOC/UserContext.tsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 import queryKeys from "../config/queryKeys";
 import { useToast } from "../ui/use-toast";
 import { IUser } from "../config/types";
-// import Logo from "@/components/ui/Logo";
+import Logo from "@/components/ui/Logo";
 import { useQuery } from "react-query";
 import useAuth from "@/hooks/useAuth";
 
@@ -17,61 +17,55 @@ export const UserContext = createContext<
 
 function UserContextProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<IUser>({} as IUser);
-  const { toast } = useToast();
-  const [ loading, setLoading ] =useState(true);
-  const { axiosClient } = useAuth();
+  // const { toast } = useToast();
+  // const { axiosClient } = useAuth();
 
-  const fetchUser = async () => {
-    return axiosClient.get("customers/me");
-  };
+  // const fetchUser = async () => {
+  //   return axiosClient.get("customers/me");
+  // };
 
-  const { data, isLoading, isError } = useQuery(
-    queryKeys.AUTH_USER_ME,
-    fetchUser,
-    {
-      cacheTime: 1000,
-      retry:false,
-    }
-  );
+  // const { data, isLoading, isError } = useQuery(
+  //   queryKeys.AUTH_USER_ME,
+  //   fetchUser,
+  //   {
+  //     cacheTime: 1000,
+  //     retry:false,
+  //   }
+  // );
 
-  useEffect(() => {
-    if (data?.data?.data) {
-      setUser(data?.data?.data);
-    }
-  }, [data]);
+  // useEffect(() => {
+  //   if (data?.data?.data) {
+  //     setUser(data?.data?.data);
+  //   }
+  // }, [data]);
 
-  useEffect(() => {
-    // if (isError) {
-    //   toast({
-    //     variant: "destructive",
-    //     title: "Authentication failed",
-    //     description: "Authentication failed",
-    //   });
-    //   if (!window.location.href.includes("auth") && window.location.href === "/") {
-    //     // window.location.href = "/auth";
-    //   }
-    // }
-  }, [isError]);
-
-  useEffect(()=>{
-    setTimeout(() => {
-      setLoading(false);
-    }, 4000);
-  }, [])
+  // useEffect(() => {
+  //   // if (isError) {
+  //   //   toast({
+  //   //     variant: "destructive",
+  //   //     title: "Authentication failed",
+  //   //     description: "Authentication failed",
+  //   //   });
+  //   //   if (!window.location.href.includes("auth") && window.location.href === "/") {
+  //   //     // window.location.href = "/auth";
+  //   //   }
+  //   // }
+  // }, [isError]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <div className="flex-1 w-full">
-        {loading || isLoading ? (
+      {/* <div className="flex-1 w-full">
+        {isLoading ? (
           <div className="fixed top-0 right-0 left-0 bottom-0 bg-white flex justify-center items-center">
             <div className="animate-pulse">
-              <img  src="/images/logo.png" />
+              <Logo />
             </div>
           </div>
         ) : (
           children
         )}
-      </div>
+      </div> */}
+      {children}
     </UserContext.Provider>
   );
 }
