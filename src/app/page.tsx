5c95f23b806c36798e3d49f065471f1fa52312bf
Chuqdi <morganhezekiah111@gmail.com>
2024-09-27 17:47:35 +0100

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,7 +116,7 @@ export default function Main() {
       />
       <Navbar />
       <div className="flex flex-col-reverse md:flex-row my-32 mb-8 md:mb-32 items-center">
-        <div className="flex-1 p-2 md:p-0 md:ml-[2rem] flex flex-col gap-5">
+        <div className="w-full  p-2  md:p-0 md:ml-[3rem] flex flex-col gap-5">
           <div>
             <h2 className="text-[#030517] font-NewSpiritBold text-[2.5rem] md:text-[4.5rem] md:leading-[5.85rem] md:tracking-[-0.135rem]">
               Savor the Flavors with Nourisha
@@ -150,7 +150,7 @@ export default function Main() {
           <img src="/images/taste.png" className="w-full z-[999]" />
         ) : (
          
-          <img src="/images/taste.png" className="flex-1  w-[40.1875rem] h-[23.35225rem] z-[999]" />
+          <img src="/images/taste.png" className=" w-[37.1875rem] h-[20.35225rem] z-[999]" />
         )}
       </div>
 
