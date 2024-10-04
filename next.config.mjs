88400ee92b7803da0a865ad7ed9704e699d721ca
Chuqdi <morganhezekiah111@gmail.com>
2024-10-04 13:23:00 +0100

    g

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,8 @@ const nextConfig = {
   env: {
     // API_URL: "https://api-stage.eatnourisha.com/v1/",
     API_URL: "https://api.eatnourisha.com/v1/",
+
+    
     STRIPE_PK_TEST: "pk_test_CTdNlDzkd3JhDT8yof3Hdw5B",
   },
 };
