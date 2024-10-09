5fb1a58d11257048af1d72debf5e2b74c0b14918
Chuqdi <morganhezekiah111@gmail.com>
2024-10-09 12:29:06 +0100

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,7 +125,7 @@ export default function Main() {
         className=" absolute right-0 top-0 h-[32.5rem] md:h-[60.0625rem] w-full md:w-[50rem] z-0 "
       />
       <Navbar />
-      <div className="flex flex-col-reverse md:flex-row my-32 mb-8 md:mb-32 items-center">
+      <div className="flex flex-col-reverse md:flex-row my-32 mb-8 md:mb-32 items-center md:items-start">
         <div className="w-full  p-2  md:p-0 md:ml-[3rem] flex flex-col gap-5">
           <div>
             <h2 className="text-[#030517] font-NewSpiritBold text-[2.5rem] md:text-[4.5rem] md:leading-[5.85rem] md:tracking-[-0.135rem]">
@@ -164,7 +164,7 @@ export default function Main() {
           ) : (
             <img
               src="/images/taste.png"
-              className={`w-[40.1875rem] h-auto ${cartLoading?"z-0":" z-10"}`}
+              className={`w-[38.1875rem] h-[23rem] ${cartLoading?"z-0":" z-10"}`}
             />
           ))}
       </div>
