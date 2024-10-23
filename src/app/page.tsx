1801aa822478cbc607d99031f4a584cdb8391142
Chuqdi <morganhezekiah111@gmail.com>
2024-10-23 11:12:31 +0100

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,6 @@ export default function Main() {
 
   useEffect(() => {
     const cartWasOpen = localStorage.getItem(CART_MODAL_OPEN);
-    alert(cartWasOpen)
     if (cartWasOpen && cartWasOpen === "1") {
       setSideModal({ show: true, component: <CartModal /> });
     }
